5d8b69975dd0530e85d15a23d86aec3638543c1e
Christian Lohmann <christian.lohmann@zalando.de>
2016-05-19 11:58:27 +0200

    #30 added scm-source commands before building go images

--- build-all.sh
+++ build-all.sh
@@ -11,7 +11,9 @@ cd $(dirname $0)
 [ -z "$VERSION" ] && VERSION=0-SNAPSHOT
 
 # build Docker images
+scm-source -f server/scm-source.json
 docker build -f server/Dockerfile -t registry-write.opensource.zalan.do/stups/go-server:$VERSION server
+scm-source -f agent/scm-source.json
 docker build -f agent/Dockerfile -t registry-write.opensource.zalan.do/stups/go-agent:$VERSION agent
 
 # build toolchains
